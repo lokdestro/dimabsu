ed4d6245812ac6b63b70449e18446cff10ec6806
lokdestro <tishkovdima05@gmail.com>
2022-12-05 21:32:37 +0300

    calculator default 1

---
+++ calculator/Source.cpp
@@ -0,0 +1,21 @@
+#include "defoult.h"
+using std::cin;
+using std::cout;
+std::map<std::string, void(*)()> selects{ {"defoult",defoult} };
+
+int main() {
+	//while (1) {
+		cout << "choose the mode\n";
+		std::string s;
+		cin >> s;
+		if (selects.count(s)) {
+			selects[s]();
+		}
+		else {
+			cout << "wrong mode";
+		}
+		//selects["defoult"]
+		//cout << selects.count(s);
+	//}
+	return 0;
+}
---
+++ calculator/defoult.h
@@ -0,0 +1,14 @@
+#pragma once
+#include<iostream>
+#include <string>
+#include <map>
+using std::cin;
+
+double add(double, double);
+double multiplication(double, double);
+double minus(double, double);
+double square(double);
+double squareRoot(double);
+double reverseElement(double);
+double division(double,double);
+void defoult();
