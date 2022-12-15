1780b905bb07f47bb4d915fe80bfdbf8942329db
lokdestro <tishkovdima05@gmail.com>
2022-12-15 19:09:17 +0300

    calculator with trig functions

--- calculator/Source.cpp
+++ calculator/Source.cpp
@@ -1,21 +1,22 @@
 #include "defoult.h"
+#include "scientific.h"
 using std::cin;
 using std::cout;
 std::map<std::string, void(*)()> selects{ {"defoult",defoult} };
 
+
 int main() {
-	while (1) {
-		cout << "choose the mode\n";
-		std::string s;
-		cin >> s;
-		if (selects.count(s)) {
-			selects[s]();
-		}
-		else {
-			cout << "wrong mode";
-		}
-		//selects["defoult"]
-		//cout << selects.count(s);
-	}
+	//while (1) {
+	//	cout << "choose the mode\n";
+	//	std::string s;
+	//	cin >> s;
+	//	if (selects.count(s)) {
+	//		selects[s]();
+	//	}
+	//	else {
+	//		cout << "wrong mode";
+	//	}
+	//}
+	cout << myarctg(1);
 	return 0;
 }
---
+++ calculator/scientific.h
@@ -0,0 +1,7 @@
+#pragma once
+
+double mysin(double value);
+double mycos(double value);
+double myarcsin(double value);
+double myarccos(double value);
+double myarctg(double value);
